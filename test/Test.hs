c346c709af99278d1b4439366a014370c7d3408f
True Ghiassi <true@ghiassitrio.co.uk>
2017-09-14 19:40:13 +0100

    Switching to Python.  Haskell just does not have nearly so much available for neural networks.

--- test/Test.hs
+++ test/Test.hs
@@ -1,5 +1,11 @@
 -- Copyright 5-o (c) 2017
 -- All rights reserved
 
+import qualified Test.Tasty as TT
+import qualified TestObstacleMap as Om
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = TT.defaultMain tests
+
+tests :: TT.TestTree
+tests = TT.testGroup "" Om.tests 
