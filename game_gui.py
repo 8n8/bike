e54c685bc0e051ec3ee8122e7ba9cad9b7b3131e
True Ghiassi <true@ghiassitrio.co.uk>
2018-12-31 17:22:40 +0000

    Accidentally damaged game_gui.py. Reverting.

--- game_gui.py
+++ game_gui.py
@@ -119,7 +119,7 @@ def main(
     """
     root = k.Tk()
     root.title('Robot navigation simulator')
-    root.geometry('800x600')
+    root.geometry('80A privacy reminder from Google
     canvas = k.Canvas(root, width=650, height=1000, bg='grey')
     canvas.pack()
 
